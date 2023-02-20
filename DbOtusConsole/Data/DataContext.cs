57e43b5b6015f2518de41cfd6e25b078ffbd68c1
seriktest <96250381+seriktest@users.noreply.github.com>
2023-02-20 11:06:16 +0300

    init

--- DbOtusConsole/Data/DataContext.cs
+++ DbOtusConsole/Data/DataContext.cs
@@ -6,8 +6,52 @@ namespace DbOtusConsole.Data;
 public class DataContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
     
-    public DataContext(DbContextOptions<DataContext> options): base(options)
+    public DataContext(DbContextOptions<DataContext> options): base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseNpgsql("User ID=postgres;Port=5432;Host=localhost;Password=123;Database=otus");
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(opt =>
+        {
+            opt.ToTable("users")
+                .HasKey(x => x.Id);
+            opt.Property(x => x.Id).HasColumnName("id");
+            opt.Property(x => x.FirstName).HasColumnName("first_name")
+                .IsRequired();
+            opt.Property(x => x.LastName).HasColumnName("last_name");
+            opt.Property(x => x.Email).HasColumnName("email")
+                .IsRequired();
+        });
+
+        modelBuilder.Entity<Product>(opt =>
+        {
+            opt.ToTable("produscts")
+                .HasKey(x => x.Id);
+            opt.Property(x => x.Id).HasColumnName("id");
+            opt.Property(x => x.Title).HasColumnName("title")
+                .IsRequired();
+            opt.Property(x => x.Description).HasColumnName("description")
+                .HasMaxLength(300);
+            opt.Property(x => x.Price).HasColumnName("price");
+
+            opt.HasOne(u => u.User)
+                .WithMany(p => p.Products);
+        });
+            
+            
+
+
+
+
+
+
+        base.OnModelCreating(modelBuilder);
     }
 }
---
+++ DbOtusConsole/Entity/Product.cs
@@ -0,0 +1,11 @@
+﻿using DbOtusConsole.Abstractions;
+
+namespace DbOtusConsole.Entity;
+
+public class Product :BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public User User { get; set; }
+}
